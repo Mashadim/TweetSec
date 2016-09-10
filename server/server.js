ed8812c068b480307446b9b458b65ede6f15bb24
Masha Diminsky <mashadiminsky@Mashas-MacBook-Pro.local>
2016-09-09 17:46:52 -0700

    Minor updates in main.js

--- server/server.js
+++ server/server.js
@@ -1,28 +1,33 @@
+const express = require('express');
+const app = express();
 const Twit = require('../config/twit.config');
 const TweetSecBot = require('../lib/main');
 const userToTweetTo = '@philosophiepass';
+const port = 3000;
 
 var stream = Twit.stream('statuses/filter', { track: userToTweetTo });
 
 stream.on('connect', (request) => {
-	console.log('Philosophie\'s TweetSec connecting...')
+	console.log('Philosophie\'s TweetSec connecting...');
 });
 
 // 'tweet' event not capturing any response even when tweeted
 stream.on('tweet', (response) => {
-	console.log('in tweet')
-	var username = response.user.screen_name;
+	var username = response.user.screen_name; // need to change this
 	var password = response.text.replace(userToTweetTo, '');	
 	
+	// Works
 	TweetSecBot.checkPasswordStrength(password)
-	.then((result) => {
-		console.log('Done!', result)
+		.then((result) => {
+			console.log('Done!', result)
 
-		Twit.post('statuses/update', { status: `Hey ${username}, your password is ${result}.` }, (err, data, response) => {
-			if(err) console.log(err)
+			Twit.post('statuses/update', { status: `Hey ${username}, your password is ${result}.` }, (err, data, response) => {
+				if(err) console.log(err)
+			})
 		})
-	})
-	.catch((err) => {
-		console.log(err)
-	})
+		.catch((err) => {
+			console.log(err)
+		})
 });
+
+app.listen(port, () => console.log('Listening on port 3000'));
